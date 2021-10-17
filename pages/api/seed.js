e157b8ab242cdc35ca13a35e33d48cb075bb5b47
breezy <apelibrian@gmail.com>
2021-10-17 11:48:11 +0300

    Fetch data from API

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -12,5 +12,5 @@ handler.get(async(req,res) => {
     await Product.insertMany(data.products);//insert document / array of products
     await db.disconnect();
     res.send({message: 'seeded successfully'});
-})
+});
 export default handler;
