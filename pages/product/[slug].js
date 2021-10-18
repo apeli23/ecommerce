b81bc2ace9574ffa72f6e979999096df8794f4ee
breezy <apelibrian@gmail.com>
2021-10-18 09:34:25 +0300

    Add products to shopping cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {useRouter} from 'next/router';
 import data from '../../utils/data';
 import Layout from '../../components/Layout';
@@ -6,8 +6,11 @@ import Link from 'next/link';
 import useStyles from '../../utils/styles';
 import {Grid, List, ListItem, Typography, Card, Button} from '@material-ui/core';
 import Image from 'next/image';
+import axios from 'axios';
+import {Store}from '../../utils/Store'
 
 export default function ProductScreen() {
+    const{dispatch} = useContext(Store);
     const classes = useStyles();
     const router = useRouter();
     const {slug} = router.query;
@@ -15,6 +18,14 @@ export default function ProductScreen() {
 
     if(!product) {
         return <div>Product Not Found!</div>
+    }
+    const addToCartHandler = async () => {
+        const {data} = await axios.get(`/api/products/${product._id}`);
+        if(data.countInStock <=0) {
+            window.alert('Sorry. Product is out of Stock');
+            return
+        }
+        dispatch({type: 'CART_ADD_ITEM', payload: {...product, quantity:1}})
     }
     return (
         <Layout title ={product.name} description={product.description}>
@@ -69,7 +80,12 @@ export default function ProductScreen() {
                                 </Grid>
                             </ListItem>
                             <ListItem>
-                                <Button fullWidth variant="contained" color="primary">
+                                <Button 
+                                    fullWidth 
+                                    variant="contained" 
+                                    color="primary"
+                                    onClick={addToCartHandler}
+                                >
                                 Add to cart
                                 </Button>
                             </ListItem>
