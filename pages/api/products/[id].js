523c0023e6e0efb1ba8c1ed200667a7eedbca442
breezy <apelibrian@gmail.com>
2021-10-19 14:41:16 +0300

    Create shopping cart

---
+++ pages/api/products/[id].js
@@ -0,0 +1,14 @@
+// Implement API for '/api/product[id]'
+import nc from 'next-connect';
+import Product from '../../../models/Product';
+import db from '../../../utils/db'
+
+const handler = nc();
+
+handler.get(async(req,res) => {
+    await db.connect();
+    const product = await Product.findById(req.query.id); //return all products without any filter
+    await db.disconnect();
+    res.send(product);
+})
+export default handler;
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Layout from '../../components/Layout';
 import Link from 'next/link';
 import useStyles from '../../utils/styles';
 import {Grid, List, ListItem, Typography, Card, Button} from '@material-ui/core';
 import Image from 'next/image';
 import db from '../../utils/db';
-import Product from '../../models/Product'
+import {Store} from '../../utils/Store';
+import Product from '../../models/Product';
+import axios from 'axios';
+import {useRouter} from 'next/router';
 
 export default function ProductScreen(props) {
+    const router = useRouter();
+    const {dispatch} = useContext(Store);
     const {product} = props;
     const classes = useStyles();
 
     if(!product) {
         return <div>Product Not Found!</div>
+    }
+    const addToCartHandler = async() => {
+        const {data} = await axios.get(`/api/products/${product._id}`);
+        if (data.countInStock <=0){
+            window.alert('Sorry. Product is out of stock. Please try again.');
+            return;
+        }
+        dispatch({type: "CART_ADD_ITEM", payload: {...product, quantity: 1 }});
+        router.push('/cart');
     }
     return (
         <Layout title ={product.name} description={product.description}>
@@ -37,7 +51,7 @@ export default function ProductScreen(props) {
                         <ListItem><Typography>Brand: {product.brand}</Typography></ListItem>
                         <ListItem><Typography>Rating: {product.rating} stars({product.numReviews} reviews)</Typography></ListItem>
                         <ListItem>
-                            <Typography>Description:{' '}{product.description}</Typography>
+                            <Typography>Description :{product.description}</Typography>
                         </ListItem>
                     </List>
                 </Grid>
@@ -67,7 +81,12 @@ export default function ProductScreen(props) {
                                 </Grid>
                             </ListItem>
                             <ListItem>
-                                <Button fullWidth variant="contained" color="primary">
+                                <Button 
+                                    fullWidth 
+                                    variant="contained" 
+                                    color="primary"
+                                    onClick={addToCartHandler}
+                                >
                                 Add to cart
                                 </Button>
                             </ListItem>
